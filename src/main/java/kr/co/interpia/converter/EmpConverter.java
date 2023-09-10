5905eb4b516d10f249d6b78eb77adc18a8ab812c
Ragoha <ragoha92@naver.com>
2023-09-10 09:25:19 +0900

    feat. Emp, Pjt, Skill 도메인 추가

--- src/main/java/kr/co/interpia/converter/EmpConverter.java
+++ src/main/java/kr/co/interpia/converter/EmpConverter.java
@@ -9,23 +9,23 @@ import kr.co.interpia.dto.EmpDTO;
 
 public class EmpConverter {
 
-	public static EmpDTO convertToDTO(Emp emp) {
-		return EmpDTO.builder()
-				.empId(emp.getEmpId())
-				.empPw(emp.getEmpPw())
-				.empNm(emp.getEmpNm())
-				.build();
-	}
-	
-	public static Emp conveertToModel(EmpDTO empDTO) {
-		return Emp.builder()
-				.empId(empDTO.getEmpId())
-				.empPw(empDTO.getEmpPw())
-				.empNm(empDTO.getEmpNm())
-				.build();
-	}
-	
-	public static List<EmpDTO> convertToDTOList(List<Emp> EmpList){
-		return EmpList.stream().map(EmpConverter::convertToDTO).collect(Collectors.toList());
-	}
+//	public static EmpDTO convertToDTO(Emp emp) {
+//		return EmpDTO.builder()
+//				.empId(emp.getEmpId())
+//				.empPw(emp.getEmpPw())
+//				.empNm(emp.getEmpNm())
+//				.build();
+//	}
+//	
+//	public static Emp conveertToModel(EmpDTO empDTO) {
+//		return Emp.builder()
+//				.empId(empDTO.getEmpId())
+//				.empPw(empDTO.getEmpPw())
+//				.empNm(empDTO.getEmpNm())
+//				.build();
+//	}
+//	
+//	public static List<EmpDTO> convertToDTOList(List<Emp> EmpList){
+//		return EmpList.stream().map(EmpConverter::convertToDTO).collect(Collectors.toList());
+//	}
 }
--- src/main/java/kr/co/interpia/domain/Emp.java
+++ src/main/java/kr/co/interpia/domain/Emp.java
@@ -14,7 +14,29 @@ import lombok.ToString;
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Emp {
-	String empId = "";
-	String empPw = "";
-	String empNm = "";
+	
+	private String empCd = "";			//사원 코드
+	private String empId = "";			//사원 아이디
+	private String empPw = "";			//사원 패스워드
+	private String korNm = "";			//한글 이름
+	private String engNm = "";			//영어 이름
+	private String chaNm = "";			//한문 이름
+	private String regNb = "";			//주민등록번호
+	private String imgNm = "";			//사진 이름
+	private String imgPath = "";		//사진 경로
+	private String birthDt = "";		//생년월일
+	private String birthTp = "";		//생일 타입
+	private String gender = "";			//성별
+	private String matrial = "";		//결혼유무
+	private String year = "";			//연차
+	private String salTp = "";			//급여지급유형
+	private String desJob = "";			//희망직무
+	private String joinTp = "";			//입사유형
+	private String addr = "";			//주소
+	private String addrDtl = "";		//상세주소
+	private String contact = "";		//연락처
+	private String email = "";			//이메일
+	private String techLv = "";			//기술등급
+	private String drink = "";			//주량
+	
 }
